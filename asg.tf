72e7554c588d7802528713c714961adc7217cafd
wipluv <60206056+wipluv@users.noreply.github.com>
2020-03-09 20:35:12 -0400

    Update asg.tf

--- asg.tf
+++ asg.tf
@@ -8,5 +8,5 @@ resource "aws_autoscaling_group" "default" {
   force_delete              = true
   launch_configuration      = "${aws_launch_configuration.default.name}"
   vpc_zone_identifier       = ["subnet-fb8428b6"]
-  target_group_arns         = ["${aws_lb_target_group.default.arn}"]
+ # target_group_arns         = ["${aws_lb_target_group.default.arn}"]
 }
